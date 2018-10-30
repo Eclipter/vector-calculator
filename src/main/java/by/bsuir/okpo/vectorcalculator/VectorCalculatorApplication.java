9c5fc3443355d9ab4b91029d0757538b6624ab62
Vladislav Dektiarev <eclipter97@gmail.com>
2018-10-31 01:29:28 +0300

    Add spring boot app class

---
+++ src/main/java/by/bsuir/okpo/vectorcalculator/VectorCalculatorApplication.java
@@ -0,0 +1,12 @@
+package by.bsuir.okpo.vectorcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorCalculatorApplication.class, args);
+	}
+}
