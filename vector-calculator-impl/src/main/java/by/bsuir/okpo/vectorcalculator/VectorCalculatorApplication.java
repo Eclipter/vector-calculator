de9d07eb3d609af7492efcfe0e51090c15c66aad
Vladislav Dektiarev <Vladislav_Dektiarev@epam.com>
2018-11-01 18:49:17 +0300

    Setup load balancer for application

--- vector-calculator-impl/src/main/java/by/bsuir/okpo/vectorcalculator/VectorCalculatorApplication.java
+++ vector-calculator-impl/src/main/java/by/bsuir/okpo/vectorcalculator/VectorCalculatorApplication.java
@@ -2,8 +2,10 @@ package by.bsuir.okpo.vectorcalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VectorCalculatorApplication {
 
 	public static void main(String[] args) {
